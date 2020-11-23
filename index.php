6e1b21337810a239afddc1b62e204a75932f55d6
Syfanur <syfanur31@gmail.com>
2020-11-23 22:41:17 +0700

    progress minggu 9

---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  <!-- Head -->
+  <head>
+    <title>Aplikasi Logistik</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="keywords" content="Gym website, Bootstrap Theme, Freebies, UI Kit, MIT license">
+    <meta name="description" content="Bootstrap 4 Template by WebThemez">
+    <meta name="author" content="webthemez.com">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+
+    <!-- Bootstrap Styles -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+
+    <!-- Vendor Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/font-awesome/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick-carousel/slick.css"> 
+    <link rel="stylesheet" type="text/css" href="assets/fonts/themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/magnific-popup/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/hovereffects.css"> 
+    <!-- Theme Styles -->
+    <link href="assets/css/scrolling-nav.css" rel="stylesheet">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/theme.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+
+    <style type="text/css">
+ .box {
+  width:  350px;
+  height: 100px;
+  background-color: #F4A460;
+  text-align: center;
+  line-height: 30px;
+  margin: 5px;
+ }
+ 
+ .clear {clear: both;}
+</style>
+  </head>
+  <!-- End Head -->
+
+  <body>
+    <!-- Header -->
+    <header id="top">
+      <!-- Navbar --> 
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top" id="mainNav">
+		<div class="container">
+		  <a class="navbar-brand js-scroll-trigger" href="#page-top">Aplikasi<span>Logistik</span></a>  
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="ti-menu"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link js-scroll-trigger" href="#top">Home</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link js-scroll-trigger" href="#services">Tracking Barang</a>
+			  </li>
+			   
+			  <li class="nav-item">
+				<a class="nav-link js-scroll-trigger" href="#contact">About</a>
+			  </li>
+			</ul>
+		  </div>
+		</div>
+	  </nav>
+      <!-- End Navbar --> 
+		<section class="hero-large hero">
+			<div id="demo" class="carousel slide" data-ride="carousel">
+			  <ul class="carousel-indicators">
+				<li data-target="#demo" data-slide-to="0" class="active"></li>
+				<li data-target="#demo" data-slide-to="1"></li>
+			  </ul>
+			  <div class="carousel-inner">
+		
+				<div class="carousel-item active">
+				  <img src="assets/img/intro/background.jpg" alt="Chicago" width="1100" height="500">
+				    <div class="container">
+				  <div class="carousel-caption">
+					<h2>Aplikasi Logistik</h2>
+          <p style="font-size:18px">Aplikasi logistik ini berfungsi untuk memantau data pengiriman dan penerimaan barang dari divisi procurement</p>
+				  </div>   
+				</div>
+				</div>
+				<div class="carousel-item">
+				  <img src="assets/img/intro/background2.jpg" alt="New York" width="1100" height="500">
+				    <div class="container">
+				  <div class="carousel-caption">
+            <h2>Aplikasi Logistik</h2>
+            <p style="font-size:18px">Aplikasi ini juga dapat digunakan untuk tracking barang hingga sampai di gudang tujuan</p>
+				  </div>   
+				  </div>
+				</div>
+			  </div>
+			  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+			  </a>
+			  <a class="carousel-control-next" href="#demo" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+			  </a>
+			</div>
+			
+		</section>
+
+    </header>
+    <!-- End Header -->
+
+    <main role="main" class="mt-lg-5 m-0">
+	
+	<section class="wt-section" id="services">
+        <div class="container">
+		<div class="row justify-content-md-center text-center pb-lg-4 mb-lg-5 mb-4">
+          <div class="col-md-8 text-center w-md-50 mx-auto mb-0">
+            <h2 class="mb-md-2">Tracking Barang</h2>
+          </div>
+		</div> 
+          
+
+ 
+      <div class="col-md-12 col-sm-12 col-xs-12">
+
+          <div class="panel panel-default">
+              <div class="panel-heading">
+               <b>Data Tracking Barang</b> 
+                
+              </div> 
+              <br>
+              <div class="panel-body">
+                  <div class="table-responsive">
+                    <?php
+
+                $url = file_get_contents('https://loogistik.000webhostapp.com/logistik.json');
+                $data = json_decode($url, true);
+
+                ?>
+                      <table class="table table-striped table-bordered table-hover">
+                          <thead>
+                              <tr>
+                                  <th>Tanggal Pengiriman</th>
+                                  <th>Durasi Pengiriman</th>
+                                  <th>Nama Barang</th>
+                                  <th>No Resi</th>
+                                  <th>Pengirim</th>
+                                  <th>Lokasi Barang</th>
+                                  <th>Status</th>
+                                  <th>Tujuan</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                          <?php 
+
+                    foreach ($data as $dataa):
+                    ?>
+
+                    <tr>
+                     <td><?=$dataa['Tanggal_Pengiriman']?></td>
+                     <td><?=$dataa['Duras_Pengiriman']?></td>
+                     <td><?=$dataa['Nama_Barang']?></td>
+                     <td><?=$dataa['No_Resi']?></td>
+                     <td><?=$dataa['Nama_Pengirim']?></td>
+                     <td><?=$dataa['Lokasi_Barang']?></td>
+                     <td><?=$dataa['Status']?></td>
+                     <td><?=$dataa['Tujuan_Pengiriman']?></td>
+                    </tr>
+
+                    <?php 
+                    endforeach 
+                    ?>
+
+                          </tbody>
+                      </table>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+  </div>
+  <!-- /. ROW  -->
+  
+        </div>
+      </section> 
+
+  
+
+<section class="wt-section bg-light" id="contact">
+    <div class="container">
+      <center><h2 class="mb-md-2">Address</h2></center>
+      <br><br>
+		 <div class="row" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.2986422225986!2d107.62834001477327!3d-6.974049293874376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6285c5b7da517%3A0x864485f26a388f95!2sTelkom%20University!5e0!3m2!1sen!2sid!4v1604131088146!5m2!1sen!2sid" width="1300" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    </div>
+</section> 
+      <!-- End Clients Section -->
+    </main>
+
+    <!-- Footer -->
+    <footer class="bg-dark py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 text-center text-md-left mb-3 mb-md-0">
+            <small class="text-white"><a class="text-white" href="https://webthemez.com/free-bootstrap-templates/">&copy; All Rights Reserved</small>
+          </div>
+
+          <div class="col-md-4 align-self-center">
+            <ul class="list-inline text-center text-md-right mb-0">
+              <li class="list-inline-item mx-2" data-toggle="tooltip" data-placement="top" title="Facebook">
+                <a class="text-white" target="_blank" href="https://www.facebook.com/">
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item mx-2" data-toggle="tooltip" data-placement="top" title="Instagram">
+                <a class="text-white" target="_blank" href="https://www.instagram.com/">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item mx-2" data-toggle="tooltip" data-placement="top" title="Twitter">
+                <a class="text-white" target="_blank" href="https://twitter.com/">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item mx-2" data-toggle="tooltip" data-placement="top" title="Dribbble">
+                <a class="text-white" target="_blank" href="https://dribbble.com/">
+                  <i class="fab fa-dribbble"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer -->
+  
+    <!-- JS Script Files --> 
+    <script src="assets/vendors/jquery.min.js"></script>
+    <script src="assets/vendors/jquery.migrate.min.js"></script>
+    <script src="assets/vendors/popper.min.js"></script>
+    <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/vendors/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Components Vendor  --> 
+    <script src="assets/vendors/slick-carousel/slick.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.js"></script>
+    <script src="assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="assets/vendors/counters/waypoint.min.js"></script>
+    <script src="assets/vendors/counters/counterup.min.js"></script>
+
+	<script src="assets/contact/jqBootstrapValidation.js"></script>
+	<script src="assets/contact/contact_me.js"></script>
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    <!--Plugin Initialize-->
+    <script src="assets/js/global.js"></script>
+
+    <!-- Theme Components and Settings --> 
+    <script src="assets/vendors/carousel.js"></script>
+    <script src="assets/vendors/counters.js"></script>
+
+    <script>
+     function play(){
+       var audio = document.getElementById("audio");
+       audio.play();
+}
+</script>
+
+	
+  </body>
+  <!-- End Body -->
+</html>
+
+
