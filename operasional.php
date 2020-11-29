7378a507059ce15ee47008945c633050cca9270b
unknown <nurulfarikhah386@gmail.com>
2020-11-29 14:56:43 +0700

    operasional

--- operasional.php
+++ operasional.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
-<html lang="en" class="no-js">
-  <!-- Head -->
-  <head>
-    <title>Aplikasi Logistik</title>
+<html>
+<head>
+  <title>OPERASIONAL</title>
 
     <!-- Meta -->
     <meta charset="utf-8"/>
@@ -23,93 +22,104 @@
     <link rel="stylesheet" type="text/css" href="assets/vendors/slick-carousel/slick.css"> 
     <link rel="stylesheet" type="text/css" href="assets/fonts/themify-icons.css">
     <link rel="stylesheet" type="text/css" href="assets/vendors/magnific-popup/magnific-popup.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/hovereffects.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/hovereffects.css"> 
     <!-- Theme Styles -->
     <link href="assets/css/scrolling-nav.css" rel="stylesheet">
-	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="assets/css/theme.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/css/theme.css">
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 
+    <style type="text/css">
+ .box {
+  width:  350px;
+  height: 100px;
+  background-color: #F4A460;
+  text-align: center;
+  line-height: 30px;
+  margin: 5px;
+ }
+ 
+ .clear {clear: both;}
 </style>
   </head>
   <!-- End Head -->
-
-  <body>
+ <body>
     <!-- Header -->
     <header id="top">
       <!-- Navbar --> 
-	  <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top" id="mainNav">
-		<div class="container">
-		  <a class="navbar-brand js-scroll-trigger" href="#page-top">Aplikasi<span>Logistik</span></a>  
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="ti-menu"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarResponsive">
-			<ul class="navbar-nav ml-auto">
-			<li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#top">Home</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#services">Tracking Barang</a>
-			  </li>
-			   
-			  <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#contact">About</a>
-			  </li>
-			</ul>
-		  </div>
-		</div>
-	  </nav>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Aplikasi<span>Logistik</span></a>  
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="ti-menu"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#top">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="http://localhost/LogistikAPI/index.php">Tracking Barang</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#services">Operasional</a>
+        </li> 
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#contact">About</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+    </nav>
       <!-- End Navbar --> 
-		<section class="hero-large hero">
-			<div id="demo" class="carousel slide" data-ride="carousel">
-			  <ul class="carousel-indicators">
-				<li data-target="#demo" data-slide-to="0" class="active"></li>
-				<li data-target="#demo" data-slide-to="1"></li>
-			  </ul>
-			  <div class="carousel-inner">
-		
-				<div class="carousel-item active">
-				  <img src="assets/img/intro/background.jpg" alt="Chicago" width="1100" height="500">
-				    <div class="container">
-				  <div class="carousel-caption">
-					<h2>Aplikasi Logistik</h2>
+    <section class="hero-large hero">
+      <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        </ul>
+        <div class="carousel-inner">
+    
+        <div class="carousel-item active">
+          <img src="assets/img/intro/background.jpg" alt="Chicago" width="1100" height="500">
+            <div class="container">
+          <div class="carousel-caption">
+          <h2>Aplikasi Logistik</h2>
           <p style="font-size:18px">Aplikasi logistik ini berfungsi untuk memantau data pengiriman dan penerimaan barang dari divisi procurement</p>
-				  </div>   
-				</div>
-				</div>
-				<div class="carousel-item">
-				  <img src="assets/img/intro/background2.jpg" alt="New York" width="1100" height="500">
-				    <div class="container">
-				  <div class="carousel-caption">
+          </div>   
+        </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/intro/background2.jpg" alt="New York" width="1100" height="500">
+            <div class="container">
+          <div class="carousel-caption">
             <h2>Aplikasi Logistik</h2>
             <p style="font-size:18px">Aplikasi ini juga dapat digunakan untuk tracking barang hingga sampai di gudang tujuan</p>
-				  </div>   
-				  </div>
-				</div>
-			  </div>
-			  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-				<span class="carousel-control-prev-icon"></span>
-			  </a>
-			  <a class="carousel-control-next" href="#demo" data-slide="next">
-				<span class="carousel-control-next-icon"></span>
-			  </a>
-			</div>
-			
-		</section>
+          </div>   
+          </div>
+        </div>
+        </div>
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+      
+    </section>
 
     </header>
     <!-- End Header -->
-
-    <main role="main" class="mt-lg-5 m-0">
-	
-	<section class="wt-section" id="services">
+   <main role="main" class="mt-lg-5 m-0">
+  
+  <section class="wt-section" id="services">
         <div class="container">
-		<div class="row justify-content-md-center text-center pb-lg-4 mb-lg-5 mb-4">
+    <div class="row justify-content-md-center text-center pb-lg-4 mb-lg-5 mb-4">
           <div class="col-md-8 text-center w-md-50 mx-auto mb-0">
-            <h2 class="mb-md-2">Tracking Barang</h2>
+            <h2 class="mb-md-2">Operasional Logistik</h2>
           </div>
-		</div> 
+    </div> 
           
 
  
@@ -117,66 +127,48 @@
 
           <div class="panel panel-default">
               <div class="panel-heading">
-               <b>Data Tracking Barang</b> 
+               <b>Form Data Operasional</b> 
                 
               </div> 
               <br>
               <div class="panel-body">
                   <div class="table-responsive">
-                    <?php
+                   <?php
 
                 $url = file_get_contents('https://loogistik.000webhostapp.com/logistik.json');
                 $data = json_decode($url, true);
 
                 ?>
-                      <table class="table table-striped table-bordered table-hover">
-                          <thead>
-                              <tr>
-                                  <th>Tanggal Pengiriman</th>
-                                  <th>Durasi Pengiriman</th>
-                                  <th>Nama Barang</th>
-                                  <th>No Resi</th>
-                                  <th>Pengirim</th>
-                                  <th>Lokasi Barang</th>
-                                  <th>Status</th>
-                                  <th>Tujuan</th>
-                              </tr>
-                          </thead>
-                          <tbody>
-                          <?php 
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Tanggal</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Jenis Pengeluaran</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Kuantitas</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Satuan</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Harga Satuan</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 
-                    foreach ($data as $dataa):
-                    ?>
-
-                    <tr>
-                     <td><?=$dataa['Tanggal_Pengiriman']?></td>
-                     <td><?=$dataa['Duras_Pengiriman']?></td>
-                     <td><?=$dataa['Nama_Barang']?></td>
-                     <td><?=$dataa['No_Resi']?></td>
-                     <td><?=$dataa['Nama_Pengirim']?></td>
-                     <td><?=$dataa['Lokasi_Barang']?></td>
-                     <td><?=$dataa['Status']?></td>
-                     <td><?=$dataa['Tujuan_Pengiriman']?></td>
-                    </tr>
-
-                    <?php 
-                    endforeach 
-                    ?>
-
-                          </tbody>
-                      </table>
-                  </div>
-              </div>
-          </div>
-
-      </div>
-  </div>
-  <!-- /. ROW  -->
-  
-        </div>
-      </section> 
-
-  
+      <!-- End Clients Section -->
+    </main>
 
 <section class="wt-section bg-light" id="contact">
     <div class="container">
@@ -226,7 +218,8 @@
       </div>
     </footer>
     <!-- End Footer -->
-  
+
+    
     <!-- JS Script Files --> 
     <script src="assets/vendors/jquery.min.js"></script>
     <script src="assets/vendors/jquery.migrate.min.js"></script>
@@ -236,14 +229,14 @@
 
     <!-- Components Vendor  --> 
     <script src="assets/vendors/slick-carousel/slick.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.js"></script>
     <script src="assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
     <script src="assets/vendors/counters/waypoint.min.js"></script>
     <script src="assets/vendors/counters/counterup.min.js"></script>
 
-	<script src="assets/contact/jqBootstrapValidation.js"></script>
-	<script src="assets/contact/contact_me.js"></script>
-	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="assets/contact/jqBootstrapValidation.js"></script>
+  <script src="assets/contact/contact_me.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="assets/js/scrolling-nav.js"></script>
     <!--Plugin Initialize-->
     <script src="assets/js/global.js"></script>
@@ -259,9 +252,8 @@
 }
 </script>
 
-	
+  
   </body>
   <!-- End Body -->
 </html>
-
 
