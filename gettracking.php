2974d8fc4e6cddaf575e8b4395bfe6f3ff8a14e5
Syfanur <syfanur31@gmail.com>
2020-12-08 15:05:34 +0700

    update

--- gettracking.php
+++ gettracking.php
@@ -3,16 +3,21 @@
 //untuk koneksi ke db
 $koneksi = mysqli_connect('localhost','root','','logistik');
 
-$sql = "SELECT * FROM tracking";
+$sql = "SELECT * FROM kirim";
 $query = mysqli_query($koneksi, $sql);
 while ($data = mysqli_fetch_array($query)){
 
     $item[] = array(
         'id' => $data["id"],
-        'id_pengiriman' => $data["id_pengiriman"],
-        'lokasi_barang' => $data["lokasi_barang"],
-        'status' => $data["status"],
-        'timestamps' => $data["timestamps"]
+        'nama_barang' => $data["nama_barang"],
+        'jumlah_barang' => $data["jumlah_barang"],
+        'nama_vendor' => $data["nama_vendor"],
+        'tujuan_gedung' => $data["tujuan_gedung"],
+        'no_resi' => $data["no_resi"],
+        'tanggal_pengiriman' => $data["tanggal_pengiriman"],
+        'durasi' => $data["durasi"],
+        'pengirim' => $data["pengirim"],
+        'status' => $data["status"]
     );
 
     
